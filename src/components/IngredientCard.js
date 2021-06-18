40664cff08cb729d8eee2aba4c2390af8d1f2196
James Chamberlain <james@chamber-made.com>
2021-06-18 19:52:58 +0200

    feat: add buttons to filter ingredientlist results

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -3,33 +3,35 @@ import moment from 'moment'
 
 const {REACT_APP_DIGITALOCEAN_SPACE_ALIMENTS} = process.env;
 
-export default function IngredientCard({ingredient}) {
+// TODO: Need to handle when image is not retrieved correctly
 
+export default function IngredientCard({ingredient}) {
   const tags = ['vegetarian', 'vegan', 'local', 'bio']
   const badges = []
+
   tags.forEach((tag) => {
     if (ingredient[tag]) {
       const badge = <div key={tag} className='ingredient-card__badge'>{tag}</div>
       badges.push(badge)
     }
   })
+
   const creationDate = moment(ingredient.createdAt).format('DD/MM/YY')
 
   return (
     <div className='ingredient-card__main-container'>
       <img src={`${REACT_APP_DIGITALOCEAN_SPACE_ALIMENTS}/${ingredient.picture}`} alt={ingredient.name} className='ingredient-card__image'/>
       <div className='ingredient-card__content-container'>
-      <h2 className='ingredient-card__title'>
-      { ingredient.name }
-      </h2>
-      <p className='ingredient-card__body'>{ ingredient.description }</p>
+        <h2 className='ingredient-card__title'>
+          { ingredient.name }
+        </h2>
+        <p className='ingredient-card__body'>{ ingredient.description }</p>
       </div>
       <div className='ingredient-card__badge-container'>
-      {badges}
+        {badges}
       </div>
       <div className='ingredient-card__date-container'>
-
-      <p className='ingredient-card__body--date '>Created on {creationDate}</p>
+        <p className='ingredient-card__body--date '>Created on {creationDate}</p>
       </div>
     </div>
   )
--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -5,8 +5,6 @@ import {
 
 import IngredientCard from './IngredientCard.js'
 
-import {mockData} from '../queries/mockData'
-
 import { ALL_INGREDIENTS } from '../queries/ingredients.js'
 
 export default function IngredientList() {
@@ -14,47 +12,90 @@ export default function IngredientList() {
 
   const [ search, setSearch ] = useState('');
   const [ ingredients, setIngredients ] = useState([])
+  const [ ingredientCards, setIngredientCards ] = useState([])
+  const [ vegetarianActive, setVegetarianActive ] = useState(false)
+  const [ veganActive, setVeganActive ] = useState(false)
+  const [ localActive, setLocalActive ] = useState(false)
+  const [ bioActive, setBioActive ] = useState(false)
 
   useEffect(() => {
-    if (search.length) {
-      const ingredientsArr = ingredients.map((ingredient) => {
-        const clonedIngredient = { ...ingredient, category: { ...ingredient.category } }
-        return clonedIngredient;
-      })
-      const regExp = new RegExp( search, 'gi' )
-      const filteredIngredients = ingredientsArr.filter((ingredient) => {
-        return regExp.test(ingredient.name)
-      })
-      setIngredients(filteredIngredients)
-    } else {
-      if (data) setIngredients(data.aliments)
+    if (data) {
+      setIngredients(data.aliments)
+      updateIngredientCards(data.aliments)
     }
-  }, [search])
-
-  useEffect(() => {
-    if (data) setIngredients(data.aliments)
   }, [data])
 
+  useEffect(() => {
+    updateIngredientCards(ingredients)
+  }, [search, vegetarianActive, veganActive, localActive, bioActive])
 
-  const ingredientCards = ingredients.map((ingredient) => {
-    return (
-      <IngredientCard key={ingredient.id} ingredient={ingredient}></IngredientCard>
-    )
-  })
+  const updateIngredientCards = (ingredientsArr) => {
+    const clonedArr = cloneIngredientsArr(ingredientsArr);
+    const searchFilteredArr = search.length ? filterBySearch(clonedArr) : clonedArr;
+    const typesFilteredArr = filterByTypes(searchFilteredArr);
+    const updatedCards = typesFilteredArr.map((ingredient) => {
+      return (
+        <IngredientCard key={ingredient.id} ingredient={ingredient}></IngredientCard>
+      )
+    })
+    setIngredientCards(updatedCards);
+  }
+
+  const cloneIngredientsArr = (ingredientsArr) => {
+    const clonedArr = ingredientsArr.map((ingredient) => {
+      const clonedIngredient = { ...ingredient, category: { ...ingredient.category } }
+      return clonedIngredient;
+    })
+    return clonedArr;
+  }
+
+  const filterBySearch = (ingredientsArr) => {
+    const regExp = new RegExp( search, 'gi' )
+    const filteredIngredients = ingredientsArr.filter((ingredient) => {
+      return regExp.test(ingredient.name)
+    })
+    return filteredIngredients;
+  }
+
+  const filterByTypes = (ingredientsArr) => {
+    const filteredIngredients = ingredientsArr.filter((ingredient) => {
+      let isMatch = true;
+      if (vegetarianActive && ingredient.vegetarian !== true) isMatch = false
+      if (veganActive && ingredient.vegan !== true) isMatch = false
+      if (localActive && ingredient.local !== true) isMatch = false
+      if (bioActive && ingredient.bio !== true) isMatch = false
+      return isMatch;
+    })
+    return filteredIngredients;
+  }
 
   const handleSearchInput = (event) => {
     setSearch(event.target.value);
-    console.log(event.target.value)
+  }
+
+  const handleFilterButton = (filter) => {
+    if (filter === 'vegetarian') setVegetarianActive(!vegetarianActive)
+    if (filter === 'vegan') setVeganActive(!veganActive)
+    if (filter === 'local') setLocalActive(!localActive)
+    if (filter === 'bio') setBioActive(!bioActive)
   }
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: { error.message }</p>
   return (
     <div>
-    <input type="text" placeholder='Search...' onChange={(event) => handleSearchInput(event)}/>
-    <div className='ingredient-list__container'>
-    {ingredientCards}
-    </div>
+      <div className='ingredient-list__filter-container'>
+        <input type="text" placeholder='Search...' onChange={(event) => handleSearchInput(event)} className='ingredient-list__search-bar'/>
+        <div className='ingredient-list__button-container'>
+          <button onClick={() => handleFilterButton('vegetarian')} className={ vegetarianActive ? 'ingredient-list__filter-button--active' : 'ingredient-list__filter-button' }>Vegetarian</button>
+          <button onClick={() => handleFilterButton('vegan')} className={ veganActive ? 'ingredient-list__filter-button--active' : 'ingredient-list__filter-button' }>Vegan</button>
+          <button onClick={() => handleFilterButton('local')} className={ localActive ? 'ingredient-list__filter-button--active' : 'ingredient-list__filter-button' }>Local</button>
+          <button onClick={() => handleFilterButton('bio')} className={ bioActive ? 'ingredient-list__filter-button--active' : 'ingredient-list__filter-button' }>Bio</button>
+        </div>
+      </div>
+      <div className='ingredient-list__main-container'>
+        {ingredientCards}
+      </div>
     </div>
   )
 }
